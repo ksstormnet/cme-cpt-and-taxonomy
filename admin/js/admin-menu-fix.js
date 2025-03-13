b7dfd186591eb3573d6c80d3a03d5b3382e96c9e
KSStorm Dev <scott@convesio.com>
2025-03-13 10:32:26 -0500

    Fix linting issues in JavaScript and CSS files
    - Fix formatting and indentation in admin-menu-fix.js
    - Fix frontend CSS with proper empty lines between rules
    - Use modern CSS syntax (rgb with slash notation)
    - Update spacing in CSS animations
    - Auto-fix JS indentation in personas-frontend.js

--- admin/js/admin-menu-fix.js
+++ admin/js/admin-menu-fix.js
@@ -4,20 +4,26 @@
  * Since we're hiding the submenu with CSS, this script ensures
  * clicking on the main menu item correctly navigates to the personas listing.
  *
- * @package    CME_Personas
+ * @package
  * @version    1.3.0
  */
 
-jQuery(document).ready(function($) {
-    // Make the Personas menu item redirect directly to the list page
-    $('#adminmenu li.menu-top.menu-icon-persona > a.menu-top').attr('href', 'edit.php?post_type=persona');
+jQuery(document).ready(function ($) {
+	// Make the Personas menu item redirect directly to the list page
+	$('#adminmenu li.menu-top.menu-icon-persona > a.menu-top').attr(
+		'href',
+		'edit.php?post_type=persona'
+	);
 
-    // Remove any click event handlers that might interfere with direct navigation
-    $('#adminmenu li.menu-top.menu-icon-persona > a.menu-top').off('click');
+	// Remove any click event handlers that might interfere with direct navigation
+	$('#adminmenu li.menu-top.menu-icon-persona > a.menu-top').off('click');
 
-    // Add a direct click handler
-    $('#adminmenu li.menu-top.menu-icon-persona > a.menu-top').on('click', function(e) {
-        e.preventDefault();
-        window.location.href = 'edit.php?post_type=persona';
-    });
+	// Add a direct click handler
+	$('#adminmenu li.menu-top.menu-icon-persona > a.menu-top').on(
+		'click',
+		function (e) {
+			e.preventDefault();
+			window.location.href = 'edit.php?post_type=persona';
+		}
+	);
 });
